32594d53a36e0a1a23440e14ef44efdab5fe7e21
vvmk <vincentmasiello@gmail.com>
2018-06-05 00:23:28 -0400

    Finish first tutorial, begin sifting

--- src/main/java/com/complexaesthetic/userreg/UserRegApplication.java
+++ src/main/java/com/complexaesthetic/userreg/UserRegApplication.java
@@ -2,9 +2,15 @@ package com.complexaesthetic.userreg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserRegApplication {
+public class UserRegApplication extends SpringBootServletInitializer {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(UserRegApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserRegApplication.class, args);
