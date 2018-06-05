f50d08952bc69152f40406a5cb917cb49512e2af
vvmk <vincentmasiello@gmail.com>
2018-06-04 23:34:04 -0400

    Add JPA, domain entities

---
+++ src/main/java/com/complexaesthetic/userreg/domain/Role.java
@@ -0,0 +1,45 @@
+package com.complexaesthetic.userreg.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * project: user-reg
+ * package: com.complexaesthetic.userreg.domain
+ * author: https://github.com/vvmk
+ * date: 6/4/18
+ */
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
