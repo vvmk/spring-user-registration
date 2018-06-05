289a1e85e67796a73ed3cc450505365f3d01fd87
vvmk <vincentmasiello@gmail.com>
2018-06-05 00:08:59 -0400

    validation messages

---
+++ src/main/java/com/complexaesthetic/userreg/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.complexaesthetic.userreg.controller;
+
+/**
+ * project: user-reg
+ * package: com.complexaesthetic.userreg.controller
+ * author: https://github.com/vvmk
+ * date: 6/5/18
+ */
+
+public class UserController {
+}
