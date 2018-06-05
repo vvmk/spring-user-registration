6fac647b8498042501661b4f76841676ccaf4b8b
vvmk <vincentmasiello@gmail.com>
2018-06-05 00:11:12 -0400

    tentative controller

--- src/main/java/com/complexaesthetic/userreg/controller/UserController.java
+++ src/main/java/com/complexaesthetic/userreg/controller/UserController.java
@@ -1,5 +1,16 @@
 package com.complexaesthetic.userreg.controller;
 
+import com.complexaesthetic.userreg.domain.User;
+import com.complexaesthetic.userreg.service.SecurityService;
+import com.complexaesthetic.userreg.service.UserService;
+import com.complexaesthetic.userreg.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * project: user-reg
  * package: com.complexaesthetic.userreg.controller
@@ -8,4 +19,52 @@ package com.complexaesthetic.userreg.controller;
  */
 
 public class UserController {
+    private UserService userService;
+    private SecurityService securityService;
+    private UserValidator userValidator;
+
+    @Autowired
+    public UserController(UserService userService, SecurityService securityService, UserValidator userValidator) {
+        this.userService = userService;
+        this.securityService = securityService;
+        this.userValidator = userValidator;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        userService.save(userForm);
+
+        securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
+
+        return "redirect:/welcome";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
+    public String welcome(Model model) {
+        return "welcome";
+    }
 }
