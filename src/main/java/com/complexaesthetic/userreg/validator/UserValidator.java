77940017f006cfdb65de241496b564c546e42360
vvmk <vincentmasiello@gmail.com>
2018-06-05 00:04:07 -0400

    user validator

---
+++ src/main/java/com/complexaesthetic/userreg/validator/UserValidator.java
@@ -0,0 +1,53 @@
+package com.complexaesthetic.userreg.validator;
+
+import com.complexaesthetic.userreg.domain.User;
+import com.complexaesthetic.userreg.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+/**
+ * project: user-reg
+ * package: com.complexaesthetic.userreg.validator
+ * author: https://github.com/vvmk
+ * date: 6/5/18
+ */
+
+@Component
+public class UserValidator implements Validator {
+    private UserService userService;
+
+    @Autowired
+    public UserValidator(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+        if (user.getUsername().length() < 6 || user.getUsername().length() > 32) {
+            errors.rejectValue("username", "Size.userForm.username");
+        }
+        if (userService.findByUsername(user.getUsername()) != null) {
+            errors.rejectValue("username", "Duplicate.userForm.username");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+
+        if (!user.getPasswordConfirm().equals(user.getPassword())) {
+            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+        }
+    }
+}
