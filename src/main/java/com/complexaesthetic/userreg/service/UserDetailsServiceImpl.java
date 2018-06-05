2c9aad888392436f50486ba2249e305d4c20723e
vvmk <vincentmasiello@gmail.com>
2018-06-04 23:50:59 -0400

    Implement UserDetailService

---
+++ src/main/java/com/complexaesthetic/userreg/service/UserDetailsServiceImpl.java
@@ -0,0 +1,50 @@
+package com.complexaesthetic.userreg.service;
+
+import com.complexaesthetic.userreg.domain.Role;
+import com.complexaesthetic.userreg.domain.User;
+import com.complexaesthetic.userreg.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * project: user-reg
+ * package: com.complexaesthetic.userreg.service
+ * author: https://github.com/vvmk
+ * date: 6/4/18
+ */
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Role role : user.getRoles()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                grantedAuthorities
+        );
+    }
+}
